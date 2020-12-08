e0d2c12c2440e4e3e474b8e7ddcd0d0293c262d9
Khelifa Yassine <prostam27@gmail.com>
2020-12-08 23:53:23 +0100

    send mail :: P1

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,24 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+  <?php
+ 
+?>
+<script>
+ if(confirm('are u sure !')){
+     
+ }
+</script>
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+<?php
+    ini_set( 'display_errors', 1 );
+    error_reporting( E_ALL );
+    $from = $_POST['email'];
+    $to = "prostam27@gmail.com";
+           
+    $subject = $_POST['subject'];
+    $message = "Bonjour Phone-Flore \n Le client ".$_POST['name']." , il vous envoye ce message \n ( ".$_POST['message']." ). \n\n Voici le mail de votre client ".$_POST['email']." \n cdt.";
+    $headers = "De :" . $from;
+    mail($to,$subject,$message, $headers);
+    echo "L'email a été envoyé.";
+?>
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
 ?>
